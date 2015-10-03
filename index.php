0edc175792f4349f0bf806300ea9102a7744b358
Juliano Petronetto <juliano@petronetto.com.br>
2015-10-03 19:20:38 -0300

    Replaced fetch to fetchAll

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 
 require_once 'config.php';
 require_once 'Database.php';
-
-
+ 
 $conn = Database::getInstance();
 $sql = "SELECT * FROM users";	
 $stmt = $conn->prepare($sql);
 $stmt->execute();
 
-while ($obj = $stmt->fetch (PDO::FETCH_OBJ)) {
-    echo "<b>Name:</b> " . $obj->first_name . " - <b>Email:</b> " . $obj->email_id."</br>";
+$obj = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+foreach ($obj as $linha) {
+	echo "<b>Name:</b> " . $linha->first_name . " - <b>Email:</b> " . $linha->email_id."</br>";
 }
+    
